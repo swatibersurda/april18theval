6b7902847a0fe4cfd1ea442847b0a95077e0485e
swatibersurda <swatibersurda@gmail.com>
2022-04-18 16:00:24 +0530

    adding desasc

--- eval18thapriil/src/App.jsx
+++ eval18thapriil/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import { AddStudent } from "./components/AddStudent";
-import { ShowStudents } from "./components/ShowStudents";
+import { ShowStudents } from "./components/ShowStudent";
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
-      <button className="togglebtn"></button>
+      {}
+      {show ? <ShowStudents /> : <AddStudent />}
+
+      <button
+        className="togglebtn"
+        onClick={() => {
+          setShow(!setShow);
+        }}
+      >
+        {show ? "ADD a new student" : "Go to student list"}
+      </button>
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {/* make sure the table is shown initially, do not show form initially */}
       {/* make sure to show either of them do not both together */}
--- eval18thapriil/src/Components/ShowStudent.jsx
+++ eval18thapriil/src/Components/ShowStudent.jsx
@@ -1,4 +1,56 @@
+import { useEffect,useState } from "react";
+import axios from "axios";
 export const ShowStudents = () => {
+    const [databr,setdata]=useState([]);
+     console.log("databr",databr)
+
+
+   useEffect(()=>{
+       axios.get("http://localhost:8080/students").then((res)=>{
+           console.log(res.data);
+           setdata(res.data)
+        //    console.log(data);
+       }).catch((err)=>{
+           console.log(err)
+       })
+   },[])
+        
+const handleValue=(e)=>{
+    console.log(e);
+
+    console.log(e.target.value);
+    if(e.target.value==="asc")
+    {
+        const x=databr.sort((a,b)=>{
+            if(a.age>b.age)
+            {
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        })
+    }
+
+   else if(e.target.value==="desc")
+    {
+        const x=databr.sort((a,b)=>{
+            if(b.age>a.age)
+            {
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        })
+    }
+
+ setdata([...databr]);
+
+}
+
+
+
   return (
     <div>
       <div className="controls">
@@ -17,7 +69,7 @@ export const ShowStudents = () => {
         </div>
         <div>
           Order:
-          <select
+          <select onChange={handleValue}
             // select dropdown needs both value and onChange
             className="sortorder"
           >
@@ -42,7 +94,27 @@ export const ShowStudents = () => {
         </thead>
         <tbody className="tbody">
           {/* populate all rows like below: */}
-          <tr className="row">
+          
+
+
+
+
+           {databr.map((ele)=>(
+
+           <tr className="row">
+            <td className="first_name">{ele.first_name}</td>
+            <td className="last_name">{ele.last_name}</td>
+            <td className="email">{ele.email}</td>
+            <td className="gender">{ele.gender}</td>
+            <td className="age">{ele.age}</td>
+            <td className="tenth_score">{ele.tenth_score}</td>
+            <td className="twelth_score">{ele.twelth_score}</td>
+            <td className="preferred_branch">{ele.preferred_branch}</td>
+            </tr>
+
+           ))
+           }
+          {/* <tr className="row">
             <td className="first_name"></td>
             <td className="last_name"></td>
             <td className="email"></td>
@@ -51,7 +123,7 @@ export const ShowStudents = () => {
             <td className="tenth_score"></td>
             <td className="twelth_score"></td>
             <td className="preferred_branch"></td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </div>
