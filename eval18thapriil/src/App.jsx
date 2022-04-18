d327940eea67359839ae74211a88827fa1582bcd
swatibersurda <swatibersurda@gmail.com>
2022-04-18 16:37:41 +0530

    manysort

--- eval18thapriil/src/App.jsx
+++ eval18thapriil/src/App.jsx
@@ -3,20 +3,13 @@ import { AddStudent } from "./components/AddStudent";
 import { ShowStudents } from "./components/ShowStudent";
 
 function App() {
-  const [show, setShow] = useState(true);
+  const [show,setShow]=useState(true)
   return (
     <div className="App">
-      {}
-      {show ? <ShowStudents /> : <AddStudent />}
+     {}
+        {show ? <ShowStudents/> :<AddStudent/>}
 
-      <button
-        className="togglebtn"
-        onClick={() => {
-          setShow(!setShow);
-        }}
-      >
-        {show ? "ADD a new student" : "Go to student list"}
-      </button>
+      <button className="togglebtn" onClick={()=>{setShow(!setShow)}}>{show ? "ADD a new student" :"Go to student list"}</button>
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {/* make sure the table is shown initially, do not show form initially */}
       {/* make sure to show either of them do not both together */}
--- eval18thapriil/src/Components/ShowStudent.jsx
+++ eval18thapriil/src/Components/ShowStudent.jsx
@@ -1,62 +1,98 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 export const ShowStudents = () => {
-    const [databr,setdata]=useState([]);
-     console.log("databr",databr)
+  const [databr, setdata] = useState([]);
+  console.log("databr", databr);
 
-
-   useEffect(()=>{
-       axios.get("http://localhost:8080/students").then((res)=>{
-           console.log(res.data);
-           setdata(res.data)
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/students")
+      .then((res) => {
+        console.log(res.data);
+        setdata(res.data);
         //    console.log(data);
-       }).catch((err)=>{
-           console.log(err)
-       })
-   },[])
-        
-const handleValue=(e)=>{
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handelemultple=((e)=>{
+    //   console.log(e.target.value);
+    // first_name">First Name</option>
+    //         <option value="gender">Gender</option>
+    //         <option value="age">Age</option>
+    //         <option value="tenth_score">10th Score</option>
+    //         <option value="twelth_score  
+    if (e.target.value === "first_name") {
+        databr.sort((a, b) => {
+          if (a.age > b.age) {
+            return 1;
+          } else {
+            return -1;
+          }
+        });
+      }
+
+    if(e.target.value==="age")
+    {
+        databr.sort((a,b)=>{
+            return a.age-b.age;
+        })
+    }
+
+
+    if(e.target.value==="tenth_score")
+    {
+        databr.sort((a,b)=>{
+            return a.tenth_score-b.tenth_score;
+        })
+    }
+    if(e.target.value==="twelth_score")
+    {
+        databr.sort((a,b)=>{
+            return a.twelth_score-b.twelth_score;
+        })
+    }
+
+setdata([...databr])
+
+
+
+  })
+
+
+  const handleValue = (e) => {
     console.log(e);
 
     console.log(e.target.value);
-    if(e.target.value==="asc")
-    {
-        const x=databr.sort((a,b)=>{
-            if(a.age>b.age)
-            {
-                return 1;
-            }
-            else{
-                return -1;
-            }
-        })
+    if (e.target.value === "asc") {
+      const x = databr.sort((a, b) => {
+        if (a.age > b.age) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    } else if (e.target.value === "desc") {
+      const x = databr.sort((a, b) => {
+        if (b.age > a.age) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
     }
 
-   else if(e.target.value==="desc")
-    {
-        const x=databr.sort((a,b)=>{
-            if(b.age>a.age)
-            {
-                return 1;
-            }
-            else{
-                return -1;
-            }
-        })
-    }
-
- setdata([...databr]);
-
-}
-
-
+    setdata([...databr]);
+  };
 
   return (
     <div>
       <div className="controls">
         <div>
           Sort By:{" "}
-          <select
+          <select onChange={handelemultple}
             // select dropdown needs both value and onChange
             className="sortby"
           >
@@ -69,7 +105,8 @@ const handleValue=(e)=>{
         </div>
         <div>
           Order:
-          <select onChange={handleValue}
+          <select
+            onChange={handleValue}
             // select dropdown needs both value and onChange
             className="sortorder"
           >
@@ -94,26 +131,19 @@ const handleValue=(e)=>{
         </thead>
         <tbody className="tbody">
           {/* populate all rows like below: */}
-          
 
-
-
-
-           {databr.map((ele)=>(
-
-           <tr className="row">
-            <td className="first_name">{ele.first_name}</td>
-            <td className="last_name">{ele.last_name}</td>
-            <td className="email">{ele.email}</td>
-            <td className="gender">{ele.gender}</td>
-            <td className="age">{ele.age}</td>
-            <td className="tenth_score">{ele.tenth_score}</td>
-            <td className="twelth_score">{ele.twelth_score}</td>
-            <td className="preferred_branch">{ele.preferred_branch}</td>
+          {databr.map((ele) => (
+            <tr className="row">
+              <td className="first_name">{ele.first_name}</td>
+              <td className="last_name">{ele.last_name}</td>
+              <td className="email">{ele.email}</td>
+              <td className="gender">{ele.gender}</td>
+              <td className="age">{ele.age}</td>
+              <td className="tenth_score">{ele.tenth_score}</td>
+              <td className="twelth_score">{ele.twelth_score}</td>
+              <td className="preferred_branch">{ele.preferred_branch}</td>
             </tr>
-
-           ))
-           }
+          ))}
           {/* <tr className="row">
             <td className="first_name"></td>
             <td className="last_name"></td>
